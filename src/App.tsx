242ea561bd8b2e9284e24c70f4eef0db9acc635c
Admin <drew24n@gmail.com>
2020-12-22 23:01:13 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './styles/App.module.scss';
+
+function App() {
+    return (
+        <div className={style.container}>
+        </div>
+    )
+}
+
+export default App
